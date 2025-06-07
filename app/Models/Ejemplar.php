458dc1c834c33784f9e405e52a5533200fb87f64
DAW <juanmiguel.valle@iesdonana.org>
2025-06-07 11:47:25 +0200

    migraciones

---
+++ app/Models/Ejemplar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ejemplar extends Model
+{
+    /** @use HasFactory<\Database\Factories\EjemplarFactory> */
+    use HasFactory;
+}
